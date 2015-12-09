732104ef8ffbaa1aade606e4f07ee20436736147
= <=>
2015-12-09 13:56:39 -0200

    Só vai

--- deploy/lista/urls.py
+++ deploy/lista/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^gravar', views.gravar, name='gravar')
+    url(r'^gravar', views.gravar, name='gravar'),
+    url(r'^excluir', views.excluir, name='excluir')
 ]
--- deploy/lista/views.py
+++ deploy/lista/views.py
@@ -13,11 +13,17 @@ def index(request):
 
 
 def gravar(request):
-
     contato = Contatos()
     contato.nome = request.POST['nome']
     contato.email = request.POST['email']
     contato.save()
 
+    return index(request)
+
+
+def excluir(request):
+    if request.method == 'POST':
+        contato = Contatos.objects.get(codigo=request.POST['codigo'])
+        contato.delete()
 
     return index(request)
